8156688a7df228c79b7fac62e720ca04c0094c75
Areho <alejocon6907@gmail.com>
2025-06-17 17:35:25 -0500

    a

--- Set.js
+++ Set.js
@@ -2,11 +2,13 @@
 function Set(inicio, fin) {     
     let a = inicio;                      
     let b = inicio + 1;                   
-    let resultado = new Set();     
+    let resultado = new Set();  
+
     while (a <= fin) {                    
         resultado.add(a);               
         [a, b] = [b, a + b];            
     }
+    
     return Array.from(resultado);         // Convierte el Set a un arreglo y lo retorna.
 }
 console.log(Set(0, 20));       
